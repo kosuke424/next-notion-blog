f29243ff0f995dbab9e477606f51bc4b24bccc5c
Kosuke424 <kosuke24kurumilie@gmail.com>
2022-10-09 15:10:38 +0900

    seach console setting

--- next-seo.config.js
+++ next-seo.config.js
@@ -1,6 +1,12 @@
 export default {
     title: 'テクそら',
     description: 'Jamstackやweb3などのモダン技術を初心者から最短で扱えるようになるための最短経路を提供する技術情報メディアです。',
+    additionalMetaTags: [
+      {
+        name: 'google-site-verification',
+        content: 'IbWqALrYJDA863zqIjmfJPQhNG32YTnSRRUmLIvOys0',
+      }
+    ],
     openGraph: {
       type: 'website',
       locale: 'ja_JP',
